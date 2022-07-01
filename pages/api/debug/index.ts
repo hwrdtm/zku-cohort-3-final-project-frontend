d876c965f73a587056ea0a83d837dc6c1f9ffd47
hwrdtm <howard.tam.95@gmail.com>
2022-06-30 17:06:27 -0700

    remove shit

--- pages/api/debug/index.ts
+++ pages/api/debug/index.ts
@@ -6,8 +6,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // const dirToCheck = process.cwd();
-  const dirToCheck = "/vercel/path0/frontend";
+  const dirToCheck = process.cwd();
 
   const allFiles = getAllFiles(dirToCheck);
 
